18020f967d7c550d1ce7c68c5412e45b138170c6
Havard Stavnas Markhus <haavasma@stud.ntnu.no>
2023-05-05 05:41:52 +0200

    changed waiting time from setting up server to 20s

--- carla_server.py
+++ carla_server.py
@@ -36,7 +36,7 @@ def main():
 
     print("Server started on ", host, port, tm_port)
 
-    time.sleep(10)
+    time.sleep(20)
     server.stop_server()
 
 
@@ -64,7 +64,7 @@ class CarlaServer:
 
         tm_port = find_available_port()
 
-        time.sleep(10)
+        time.sleep(20)
 
         return ("127.0.0.1", port, tm_port)
 
